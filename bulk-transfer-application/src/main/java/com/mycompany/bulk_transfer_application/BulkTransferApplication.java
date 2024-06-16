aba5df038bd65c744c2a7b7f5752c940521e574d
Francesco-Bovi <francesco.bovi0510@gmail.com>
2024-06-16 18:18:46 +0200

    First commit - project initialization

---
+++ bulk-transfer-application/src/main/java/com/mycompany/bulk_transfer_application/BulkTransferApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.bulk_transfer_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulkTransferApplication.class, args);
+	}
+
+}
---
+++ bulk-transfer-application/src/test/java/com/mycompany/bulk_transfer_application/BulkTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.bulk_transfer_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
